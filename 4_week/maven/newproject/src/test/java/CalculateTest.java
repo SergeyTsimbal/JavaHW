8ce6b7958b3e362e56b00c21f4ea72ab313b1379
sergey <sfisher@ukr.net>
2017-02-17 09:50:16 +0200

    github

--- 4_week/maven/newproject/src/test/java/CalculateTest.java
+++ 4_week/maven/newproject/src/test/java/CalculateTest.java
@@ -10,28 +10,28 @@ public class CalculateTest {
     }
     @Test
     public void plus() throws Exception {
-        int res = calc.plus(1,1);
-        Assert.assertEquals(res,2);
+        int res = calc.plus(1, 1);
+        Assert.assertEquals(res, 2);
 
     }
 
     @Test
     public void minus() throws Exception {
-        int res = calc.minus(2,1);
-        Assert.assertEquals(res,1);
+        int res = calc.minus(2, 1);
+        Assert.assertEquals(res, 1);
 
     }
 
     @Test
     public void multiplied() throws Exception {
-        int res = calc.multiplied(2,2);
-        Assert.assertEquals(res,4);
+        int res = calc.multiplied(2, 2);
+        Assert.assertEquals(res, 4);
 
     }
 
     @Test
     public void divided() throws Exception {
-        double res = calc.divided(10,5);
+        double res = calc.divided(10, 5);
         System.out.println(res);
         Assert.assertTrue(res == 2);
     }
